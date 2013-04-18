44d140a57f79161edceb708b99d610416de7bd25
Richard Brown <rabrown@quantumcheese.com>
2013-04-18 10:22:05 -0700

    Renamed ViewUtils to TextViewUtils; made methods static (an oversight from earlier).

---
+++ src/com/quantumcheese/utils/TextViewUtils.java
@@ -0,0 +1,38 @@
+package com.quantumcheese.utils;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public final class TextViewUtils {
+
+    private TextViewUtils() {
+    }
+
+    private static String concatenateStrings(final String... strings) {
+        if (1 == strings.length) {
+            return strings[0];
+        } else {
+            final StringBuilder sb = new StringBuilder();
+            for (final String line : strings) {
+                sb.append(line);
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void setText(final View parent, final int targetResourceId, final String... strings) {
+        final String text = concatenateStrings(strings);
+        final TextView tv = (TextView) parent.findViewById(targetResourceId);
+        tv.setText(text);
+    }
+
+    public static void setText(final TextView tv, final String... strings) {
+        final String text = concatenateStrings(strings);
+        tv.setText(text);
+    }
+
+    public static boolean editTextIsEmpty(final EditText textField) {
+        return 0 == textField.getText().toString().length();
+    }
+}
